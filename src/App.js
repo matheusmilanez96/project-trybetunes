c8619662b4c153928bac92247e39797dd2adfdd0
Matheus Milanez <mattmila96@gmail.com>
2023-02-03 15:16:02 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <div data-testid="page-login" />
+            ) }
+          />
+          <Route
+            path="/search"
+            render={ () => (
+              <div data-testid="page-search" />
+            ) }
+          />
+          <Route
+            path="/album/:id"
+            render={ () => (
+              <div data-testid="page-album" />
+            ) }
+          />
+          <Route
+            path="/favorites"
+            render={ () => (
+              <div data-testid="page-favorites" />
+            ) }
+          />
+          <Route
+            exact
+            path="/profile"
+            render={ () => (
+              <div data-testid="page-profile" />
+            ) }
+          />
+          <Route
+            path="/profile/edit"
+            render={ () => (
+              <div data-testid="page-profile-edit" />
+            ) }
+          />
+          <Route 
+            render={ () => (
+              <div data-testid="page-not-found" />
+            ) } />
+        </Switch>
+      </div>
+    );
   }
 }
 // primeiro commit
